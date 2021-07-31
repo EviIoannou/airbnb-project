2546bc2abb281a8b2f016a1857ea707bb6b1f506
Evangelia Ioannou <evievi@outlook.com>
2021-07-31 18:19:47 +0200

    Set up react router and redux

---
+++ src/utility/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './NavBar.css';
+
+class NavBar extends Component {
+    render() {
+        return(
+            <h1 className="nav">NavBar</h1>
+        )
+    }
+}
+
+export default NavBar;

7511945793a15421a2bf8fd3885c7fe983343fd1
Evangelia Ioannou <evievi@outlook.com>
2021-08-01 11:40:34 +0200

    Setting up nav bar and search box in home page

--- src/utility/NavBar/NavBar.js
+++ src/utility/NavBar/NavBar.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
 import './NavBar.css';
+import { Link } from 'react-router-dom';
 
 class NavBar extends Component {
     render() {
+        let navColour = 'transparent';
+        if(this.props.location.pathname !== "/") {
+            navColour = 'black'
+        }
+        
         return(
-            <h1 className="nav">NavBar</h1>
+            <div className="container-fluid nav">
+                <div className="row">
+                    <nav className={navColour}>
+                        <div className="nav-wrapper">
+                            <Link to="/" className="left">airbnb</Link>
+                            <ul id="nav-mobile" className="right">
+                                <li><Link to="/">English (US)</Link></li>
+                                <li><Link to="/">$ USD</Link></li>
+                                <li><Link to="/">Become a host</Link></li>
+                                <li><Link to="/">Help</Link></li>
+                                <li><Link to="/">Sign up</Link></li>
+                                <li><Link to="/">Log in</Link></li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </div>
         )
     }
 }
---
+++ src/utility/Slider/Slider.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+// import './Slider.css';
+
+class Slider extends Component {
+    render(){
+        return(
+            <h1>Slider</h1>
+        )
+    }
+}
+
+export default Slider;
